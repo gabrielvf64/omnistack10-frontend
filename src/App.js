e7b819a5ee263cf56432f940a9c93549ca3f491d
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-18 20:07:49 -0300

    Add latitude and longitude to the state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
-import "./Main.css"
+import "./Main.css";
 
 function App() {
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
   return (
     <div id="app">
       <aside>
@@ -24,12 +45,27 @@ function App() {
           <div className="input-group">
             <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <input
+                type="number"
+                n
+                ame="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
           <button type="submit">Salvar</button>
